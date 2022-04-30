201fdeda7e732a0d60801ea23132e0a10d40cb96
mike28gt <miguelalejandro28@gmail.com>
2022-04-30 13:20:16 -0600

    Se completa ejemplo de búsqueda en anchura.

--- BusquedaEnAnchuraEnLaberintos/src/busqueda/laberinto/MotorDeBusquedaEnAnchura.java
+++ BusquedaEnAnchuraEnLaberintos/src/busqueda/laberinto/MotorDeBusquedaEnAnchura.java
@@ -34,7 +34,65 @@ public class MotorDeBusquedaEnAnchura extends MotorDeBusqueda {
         matrizEstadosVisitados[ubicacionInicio.x][ubicacionInicio.y] = true;
         cola.enqueue(ubicacionInicio);
         
+        bucleExterior:
+        while(cola.estaVacia() == false) {
+            
+            Ubicacion siguienteUbicacion = cola.front();
+            
+            if (siguienteUbicacion == null) {
+                break;
+            }
+            
+            Ubicacion[] estadosSiguientes = obtenerMovimientosPosibles(siguienteUbicacion);
+            
+            for (int i=0; i<4; i++) {
+                
+                if (estadosSiguientes[i] == null) {
+                    break;
+                }
+                
+                int coordenadaX = estadosSiguientes[i].x;
+                int coordenadaY = estadosSiguientes[i].y;
+                
+                if (matrizEstadosVisitados[coordenadaX][coordenadaY] == false) {
+                    matrizEstadosVisitados[coordenadaX][coordenadaY] = true;
+                    matrizPredecesores[coordenadaX][coordenadaY] = siguienteUbicacion;
+                    cola.enqueue(estadosSiguientes[i]);
+                    
+                    if (igual(estadosSiguientes[i], ubicacionObjetivo)) {
+                        exito = true;
+                        break bucleExterior;
+                    }
+                }
+            }
+            cola.dequeue();
+        }
+
         
+        String matriz = "";
+        for (int i=0; i<ancho; i++) {
+            for (int j=0; j<alto; j++) {
+                matriz += matrizEstadosVisitados[i][j] + "\t";
+            }
+            matriz += "\n";
+        }
+        System.out.println("--");
+        System.out.println("Matriz Estados Visitados");
+        System.out.println(matriz);
         
+        matriz = "";
+        for (int i=0; i<ancho; i++) {
+            for (int j=0; j<alto; j++) {
+                if (matrizPredecesores[i][j] != null)
+                    matriz += "(" + matrizPredecesores[i][j].x + "," + matrizPredecesores[i][j].y + ")" + "\t";
+                else
+                    matriz += "(-,-)\t";
+            }
+            matriz += "\n";
+        }
+        System.out.println("--");
+        System.out.println("Matriz de Predecesores");
+        System.out.println(matriz);
+       
     }
 }
